e942982f8ae77978af16223fe77c5d10af3d5554
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-12 14:04:29 +0530

    I have done my first program

--- Variable/Example.java
+++ Variable/Example.java
@@ -1,13 +1,9 @@
  public class Example
 {
-    public void Name(String name1,String name2)
-    {
-        String name=name1+name2;
-        System.out.println(name);
-    }
+   
     public static void main(String args[])
     {
-        Example obj=new Example();
-        obj.Name("Naruto","Uzumaki");
+       int num=27;
+       System.out.println(num);
     }
 }
