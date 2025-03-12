49fd144027320f7b3eceaabecc050a2f29ea5b4c
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-12 14:18:20 +0530

    I had done my commands

---
+++ Commands/Example1.java
@@ -0,0 +1,12 @@
+ public class Example1
+{
+    /*
+      1)demo files
+      2)using variable
+    */
+    public static void main(String args[])
+    {
+       int num=27;// This is a integer value
+       System.out.println(num);
+    }
+}
