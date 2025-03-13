44fc693f1a16d7b98965599168eb7d98cff69174
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 11:07:20 +0530

    I had done my major minor Program

---
+++ majorMinor/Majorminor.java
@@ -0,0 +1,14 @@
+public class Majorminor
+{
+    public static void main(String args[])
+    {
+        int Age=40;
+        if(Age>=18)
+        {
+            System.out.println("Major");
+        }
+        else{
+            System.out.println("Minor");
+        }
+    }
+}
