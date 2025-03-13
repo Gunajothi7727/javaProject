bf778846854b720614a6e767ad4845d767f23c37
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 11:11:59 +0530

    I had donr my Positive Negative Program

---
+++ positiveNegative/PosNeg.java
@@ -0,0 +1,15 @@
+public class PosNeg
+{
+    public static void main(String args[])
+    {
+        int number=-90;
+        if(number<0)
+        {
+            System.out.println("Negative Number");
+
+        }
+        else{
+            System.out.println("Positive Number");
+        }
+    }
+}
