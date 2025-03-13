fb2f0b68d8339b15b14c33fcbbac201d46ad9176
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 10:21:40 +0530

    I had done my Arithmetic Operator Program

---
+++ arithmeticOperator/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic
+{
+    public static void main(String args[])
+    {
+        System.out.println(10+7);
+        System.out.println(14-7);
+        System.out.println(56*7);
+        System.out.println(56/9);
+        System.out.println(45%7);
+
+    }
+}
