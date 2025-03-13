69274c135621e25efc6f1e6b2204de35f8258a53
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 10:47:46 +0530

    I had done my Ifcondition program

---
+++ conditionalStatement/Ifcondition.java
@@ -0,0 +1,14 @@
+public class Ifcondition
+{
+    public static void main(String args[])
+    {
+        if(true)
+        {
+            System.out.println("If condition is true");
+        }
+        if(false)
+        {
+            System.out.println("If condition is false");
+        }
+    }
+}
