647a0594f7c80782f3ceccae58499ea0fdea5296
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 14:25:14 +0530

    I had done my Leap year Program

---
+++ leapYear/Leap.java
@@ -0,0 +1,16 @@
+public class Leap
+{
+    public static void main(String args[])
+    {
+        int i=2020;
+        if(i%4==0)
+        {
+            System.out.println("Leap year");
+        }
+        else
+        {
+            System.out.println("Not a Leap Year");
+    }
+
+    }
+}
