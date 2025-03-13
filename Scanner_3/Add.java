05bda9e7f92b19858a40e3de74540a3695834c17
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 15:24:22 +0530

    I had done my Scanner Addition program

---
+++ Scanner_3/Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Add{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int c=sc.nextInt();
+        System.out.println("Enter the second number");
+        int d=sc.nextInt();
+        int e=c+d;
+        System.out.println(e);
+    }
+}
