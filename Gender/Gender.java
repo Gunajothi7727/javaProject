c62d93f72aefb07df30868ad257c04fbf14120fa
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 11:34:22 +0530

    I had done my Gender Program

---
+++ Gender/Gender.java
@@ -0,0 +1,15 @@
+public class Gender
+{
+    public static void main(String args[])
+    {
+        char gen='F';
+        if(gen=='F')
+        {
+            System.out.println("Female");
+        }
+        else
+        {
+            System.out.println("Male");
+        }
+    }
+}
