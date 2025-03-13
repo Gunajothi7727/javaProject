116833439c935cc0bda8c1dd00cb0e4e6d4369df
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 15:18:50 +0530

    I had done my scanner integer program

---
+++ Scanner_2/Integer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Integer{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int num=Sc.nextInt();
+        System.out.println(num);
+    }
+}
