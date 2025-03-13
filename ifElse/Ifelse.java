69f4a033313da71c1fe52b9d6d05d1052313518e
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-13 11:02:10 +0530

    I had done my Ifelse Program

---
+++ ifElse/Ifelse.java
@@ -0,0 +1,14 @@
+public class Ifelse
+{
+    public static void main(String args[])
+    {
+        if(true)
+        {
+            System.out.println("If Statement");
+        }
+        else
+        {
+            System.out.println("Else Statement");
+        }
+    }
+}
