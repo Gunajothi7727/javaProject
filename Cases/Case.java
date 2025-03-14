afa36a18f7969bb4d7bcc9d2a95de0baa6703674
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-14 12:03:35 +0530

    I had done  Cases program

---
+++ Cases/Case.java
@@ -0,0 +1,10 @@
+public class Case
+{
+    public static void main(String args[])
+    {
+        String akasuki="Pain";
+        System.out.println(akasuki.toUpperCase());
+        System.out.println(akasuki.toLowerCase());
+        
+    }
+}
