dc91443bc9132e02f30e3786dcec178c7ca365a1
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-14 09:49:05 +0530

    I haad done my Multiple command

---
+++ Command_2/Multiplecommand.java
@@ -0,0 +1,13 @@
+public class Multiplecommand
+{
+    /*
+    1)demo files
+    2)Using varaiables
+    */
+
+    public static void main(String args[])
+    {
+        int num=10;//Interger value
+        System.out.println(num);//Printing the value
+    }
+}
