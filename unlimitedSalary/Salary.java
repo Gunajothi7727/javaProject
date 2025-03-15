0e3545b62dd2de474cadf7779b3a20b4fe19b6cc
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-15 11:31:01 +0530

    Unlimited Salary

---
+++ unlimitedSalary/Salary.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Salary{
+    
+      public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        int i=1;
+        while(true)
+        {
+            System.out.println("1.Developer\n2.Manager");
+         System.out.println("Enter the Salary:");
+        int salary=sc.nextInt();
+        System.out.println("Enter the Profession");
+         i=sc.nextInt();
+        
+        if(i==1)
+        {
+            double Percent=salary*0.1;
+            double Salary1=salary+Percent;
+            System.out.println("s1"+Salary1);
+
+        }
+        else if(i==2)
+        {
+            double Percent2=salary*0.2;
+            double Salary2=salary+Percent2;
+            System.out.println("s2"+Salary2);
+        }
+        i=i+1;
+        }
+    }
+}
+
