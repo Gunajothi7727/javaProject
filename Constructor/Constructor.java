1dded9b883499ce4760f0ff82535bc2be9bdca70
Gunajothi7727 <gunajothi110@gmail.com>
2025-03-15 10:35:45 +0530

    Constructor Program

---
+++ Constructor/Constructor.java
@@ -0,0 +1,11 @@
+public class Constructor
+{
+    Constructor()
+    {
+        System.out.println("Shikamaru");
+    }
+    public static void main(String args[])
+    {
+        Constructor naruto=new Constructor();
+    }
+}
